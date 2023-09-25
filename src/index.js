b40358fae1b9a19078b8cfca6d29ac27dc044899
BalazsBanfi <banfibalazs81@gmail.com>
2023-09-25 22:01:29 +0200

    prepare forms

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import { renderPage } from "./renderPage.js";
+import { addFolder } from "./addFolder.js"
+import { addTodos } from "./addTodos.js"
 import "./style.css"
 
-console.log("Hello world!");
