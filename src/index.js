42551c702746b1bf02d06e1fd04cc5a677e80229
BalazsBanfi <banfibalazs81@gmail.com>
2023-09-26 21:57:51 +0200

    add todos to array and localstorage

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 import { renderPage } from "./renderPage.js";
-import { addFolder } from "./addFolder.js"
-import { todos } from "./addTodos.js"
-import "./style.css"
+import { addFolder } from "./addFolder.js";
+import { todos } from "./addTodos.js";
+import "./style.css";
 
 const todosList = new todos();
 
-const addTodoBtn = document.querySelector('#submit');
-addTodoBtn.addEventListener('click', () => {
-  const id = `id${Math.random().toString(16).slice(2)}`;
-  const description = document.querySelector('#description').value.trim();
-  const priority = document.querySelector('#priority').value;
-  
-  const completed = false;
-  const index = todosList.list.length + 1;
+const addTodoBtn = document.querySelector("#submit");
+addTodoBtn.addEventListener("click", () => {
+  const id = `id${String(Math.random().toString(16))}`;
+  const title = document.querySelector("#title").value.trim();
+  const description = document.querySelector("#description").value.trim();
+  const priority = document.querySelector("#priority").value;
+  const duedate = document.querySelector("#duedate").value;
 
   const newTodo = {
-    id, description, completed, index,
+    id,
+    title,
+    description,
+    priority,
+    duedate,
   };
   if (description) {
     todosList.addTodo(newTodo);
